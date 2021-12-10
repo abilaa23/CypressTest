02d28d1693a3675e5195faca4d1fdc840d5dc125
PFY-ABILA QA <rachmadhanybila@gmail.com>
2021-12-10 19:57:56 +0700

    select sub-bab under product

--- cypress/integration/0-phm-web/webpage_productmenuTest.js
+++ cypress/integration/0-phm-web/webpage_productmenuTest.js
@@ -7,9 +7,9 @@ describe('Pahamify Cypress', () => {
       cy.get('.mfp-close').click()
       cy.wait(2000)
 
-      //slide
+      //subbab product menu
       cy.get('.nav-dropdown-content > :nth-child(1) > :nth-child(5) > .nav-title')
-        .click({force: true})
+        .click()
       cy.wait(2000)
       //cy.url().should('contain', 'https://tryout.pahamify.com/')
     })
